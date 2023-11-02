6aebd8534721eb2b34d00ede7180608cb99878b1
sulaim <sulaimsaed@gmail.com>
2023-11-02 15:30:47 +0530

    Red Implemented a test function for testing punctuations

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import os
 
 
 
-def test_for_lowercase():
+def test_randomwords_for_lowercase():
     fname = "/tmp/sample_words"
     with open(fname, 'w') as f:
         f.writelines( ["Apple\n","apple\n","Banana\n"] )
@@ -14,6 +14,19 @@ def test_for_lowercase():
 
 
         assert get_random_words(fname) == "apple"
+    os.unlink(fname)
+
+
+def test_randoms_for_punctuation():
+
+    fname = "/tmp/sample_words"
+    with open(fname, 'w') as f:
+        f.writelines( ["Apple's\n","pIneapple\n","B#anana\n"] )
+
+    for _ in range(100):
+
+
+        assert get_random_words(fname) == "pIneapple"
     os.unlink(fname)
 
 
